846a03c05bd6e6bc8ce7abe46f52db17e62b98ee
commandblockguy <bluestemjc@gmail.com>
2021-04-24 01:17:18 -0500

    Optimize mining code

--- src/miner.asm
+++ src/miner.asm
@@ -7,6 +7,8 @@ sha_state = $60
 sha_block_size = $40
 sha_digest_size = 256/8
 
+bitcoin_block_size = $50
+
 macro block?
  macro end?.block?!
    .size := $-.raw_contents
@@ -29,34 +31,44 @@ end macro
 public	_mine_loop
 _mine_loop:
 	ld	iy,sha_base
-	;scf
-	;sbc	hl,hl
-	;ld	(hl),2
+
+	scf
+	sbc	hl,hl
+	ld	(hl),2
 .loop:
 	ld	hl,_block
 	lea	de,iy+sha_block
 	ld	bc,sha_block_size
 	ldir
 	ld	a,$a
-	ld	(iy),a
+	ld	(iy+sha_control),a
 
-	ld	hl,_block+sha_block_size
 	lea	de,iy+sha_block
-	ld	bc,sha_block_size
+	ld	c,bitcoin_block_size-sha_block_size
 	ldir
+	xor	a,a
+	sbc	hl,hl
+	add	hl,de	; hl = de
+	ld	(de),a
+	inc	de
+	ld	c,sha_block_size-(bitcoin_block_size-sha_block_size)
+	ldir
+	ld	de,$280 ; de = bitcoin_block_size * 8, e = $80
+	ld	(iy+sha_block+bitcoin_block_size-sha_block_size+3),e
+	ld	(iy+sha_block + sha_block_size - 4),de
 	ld	a,$e
-	ld	(iy),a
+	ld	(iy+sha_control),a
 
 	lea	hl,iy+sha_state
 	lea	de,iy+sha_block
-	ld	bc,sha_digest_size
+	ld	c,sha_digest_size
 	ldir
 	ld	a,$80
 	ld	(iy+sha_block+sha_digest_size+3),a
-	ld	b,1
+	ld	b,1 ; $bc = $100 = sha_digest_size * 8
 	ld	(iy+sha_block + sha_block_size - 4),bc
 	ld	a,$a
-	ld	(iy),a
+	ld	(iy+sha_control),a
 
 	; Compare to target
 	; No bounds check, because
@@ -98,7 +110,7 @@ public	_block
 _block:
 ; originally ends in 42a14695
 block
-	emit	$50: $0100000081cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a308000000000000e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0f1fc122bc7f5d74df2b9441a42a14695 bswap $50
+	emit	bitcoin_block_size: $0100000081cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a308000000000000e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0f1fc122bc7f5d74df2b9441a42a14695 bswap bitcoin_block_size
 end block
 .nonce := . + 76
 
